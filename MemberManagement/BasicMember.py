3536aa003a5aef212163eee55c3ce5942c740584
dld316 <72946993+dld316@users.noreply.github.com>
2022-11-02 07:01:51 -0600

    create MemberManagement branch and show project outline

---
+++ MemberManagement/BasicMember.py
@@ -0,0 +1,13 @@
+# Damon Dix
+
+"""
+This will be a basic class to create Customer member objects to be added
+to a sqlite database later
+"""
+
+
+class BasicMember:
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
